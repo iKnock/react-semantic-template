d8ceb424403133b043571136238a9daccc464aca
iKnock <ergana.henok@gmail.com>
2021-01-18 14:54:41 +0100

    init commit

---
+++ src/container/Landing.js
@@ -0,0 +1,20 @@
+import { createMedia } from '@artsy/fresnel'
+import React, { Component } from 'react'
+import {
+    Button,
+    Container,
+    Divider,
+    Grid,
+    Header,
+    Icon,
+    Image,
+    List,
+    Menu,
+    Segment,
+    Sidebar,
+    Visibility,
+} from 'semantic-ui-react'
+
+const Landing = props => {
+
+}
